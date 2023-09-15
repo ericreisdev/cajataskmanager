d06e315f3f25124b8677317070b8ff8f816a209f
ericreisdev <ericreis.dev@gmail.com>
2023-09-15 10:02:17 -0300

    final-alpha

--- src/components/Sidebar/OptionsWindow/index.jsx
+++ src/components/Sidebar/OptionsWindow/index.jsx
@@ -12,7 +12,7 @@ const OptionsWindow = ({ isOpen, onClose, selectedSpace, onNewListSubmit }) => {
       <Options>
         {selectedSpace && (
           <button onClick={() => handleOptionClick("PastaBlaster")}>
-            Lista
+            Listas
           </button>
         )}
       </Options>
